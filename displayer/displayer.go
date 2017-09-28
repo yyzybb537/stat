a4aac791607fff3549978b6174f039b7244a77a3
yuyangzi <yuyangzi@meizu.com>
2017-09-27 20:28:43 -0700

    Bytes2String

--- displayer/displayer.go
+++ displayer/displayer.go
@@ -14,3 +14,25 @@ func Printf(format string, a ... interface{}) {
 	os.Stdout.Sync()
 	displaying = len(info)
 }
+
+var suffixs = []string{"", "K", "M", "G", "T", "P", "E"}                           
+func Bytes2String(bytes int64) string {
+    var i int                                                                      
+    var dot int                                                                    
+    for i < len(suffixs) {                                                         
+        if bytes < 1024 {                                                           
+            break                                                                  
+        }                                                                          
+                                                                                   
+        //dot = bytes / 10 % 100                                                    
+        dot = int((float64(bytes)/1024.0 - float64(bytes/1024)) * 100)               
+        bytes /= 1024                                                               
+        i++                                                                        
+    }                                                                              
+                                                                                   
+    if dot > 0 {                                                                   
+        return fmt.Sprintf("%d.%02d%s", bytes, dot, suffixs[i])                     
+    }                                                                              
+                                                                                   
+    return fmt.Sprintf("%d%s", bytes, suffixs[i])                                   
+}
